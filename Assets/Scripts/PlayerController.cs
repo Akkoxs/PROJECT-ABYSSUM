b0693852c4e31ae26f9b12b1b5dc5cbc4d2515f2
ZeinAlbahrani <57455654+ZeinAlbahrani@users.noreply.github.com>
2025-12-15 12:13:14 -0500

    Underwater movement basics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,8 +36,40 @@ public class PlayerController : MonoBehaviour
     }
     #endregion
 
+
+    [Header("Underwater Physics")]
+    [SerializeField] float jumpDrag = 0.95f; // floatiness when jumping
+    [SerializeField] float horizontalAcceleration = 1.5f; // acceleration float
+    [SerializeField] float horizontalDeceleration = 0.92f; // deceleration to cause a bit of slide
+    [SerializeField] float buoyancy = 2f; // tried this out but idk if it works it kinda conflicts with the jumpdrag
+
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        float targetVelocityX = horizontal * speed;
+        float currentVelocityX = rb.linearVelocity.x;
+
+        //horizontal acceleration, decelerate if no input is detected
+        if (horizontal != 0)
+        {
+            currentVelocityX = Mathf.Lerp(currentVelocityX, targetVelocityX, horizontalAcceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentVelocityX *= horizontalDeceleration;
+        }
+
+        rb.linearVelocity = new Vector2(currentVelocityX, rb.linearVelocity.y);
+
+        //jump resistance
+        rb.linearVelocity = new Vector2(
+            rb.linearVelocity.x,
+            rb.linearVelocity.y * jumpDrag
+        );
+
+        //wanted to make the player bounce a bit then fall when jumping like ur buoyant but idk doesn't really work
+        if (!IsGrounded())
+        {
+            rb.AddForce(Vector2.up * buoyancy);
+        }
     }
 }
